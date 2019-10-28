33eb6dee38f8b310edfe0e2b44e41a61d0159886
lde <ldemianiuk@gmail.com>
2019-10-28 09:47:33 +0100

    game service

---
+++ src/app/game.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import {Shuffle} from './config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  blocks: number[];
+
+  constructor() {
+    this.blocks = [...Array(15).keys(), null];
+  }
+
+  move(n: number) {
+    for (const i of this.neighbors((n))) {
+      if (this.blocks[i] == null) {
+        this.swap(n, i);
+      }
+    }
+  }
+
+  neighbors(n: number): number[] {
+    const result = [];
+    if (n % 4 !== 0) {
+      result.push(n - 1);
+    }
+    if (n % 4 !== 3) {
+      result.push(n + 1);
+    }
+    if (n > 3) {
+      result.push(n - 4);
+    }
+    if (n < 12) {
+      result.push(n + 4);
+    }
+    return result;
+  }
+
+  isSolved(): boolean {
+    for (let i = 0; i < 15; i++) {
+      if (this.blocks[i] !== i) { return false; }
+    }
+    this.blocks[15] = 15;
+    return true;
+  }
+
+  shuffle(shuffle: Shuffle) {
+    switch (shuffle) {
+      case Shuffle.Easy:
+        this.resetArray();
+        this.swap(14, 15);
+        break;
+      case Shuffle.Random:
+        this.resetArray();
+        this.shuffleArray(this.blocks);
+        break;
+    }
+  }
+
+  private swap(n: number, i: number) {
+    const tmp = this.blocks[n];
+    this.blocks[n] = this.blocks[i];
+    this.blocks[i] = tmp;
+  }
+
+  private resetArray() {
+    for (let i = 0; i < 15; i++) {
+      this.blocks[i] = i;
+    }
+    this.blocks[15] = null;
+  }
+
+  shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+}
--- src/app/sliding-puzzle/sliding-puzzle.component.ts
+++ src/app/sliding-puzzle/sliding-puzzle.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Pipe, PipeTransform} from '@angular/core';
 import {Shuffle, ConfigService} from '../config.service';
+import {GameService} from '../game.service';
 
 @Pipe({name: 'enumToArray'})
 export class EnumToArrayPipe implements PipeTransform {
@@ -17,91 +18,35 @@ export class SlidingPuzzleComponent implements OnInit {
   four = [...Array(4).keys()];
   imagesBaseDir = 'assets/images';
   availableThemes = ['default', 'dev', 'lenna'];
-  blocks: number[];
   padding: number;
   theme: string;
   shuffle: Shuffle;
   Shuffle = Shuffle;
 
-  constructor(private config: ConfigService) {
+  constructor(private config: ConfigService,
+              private game: GameService) {
   }
 
   ngOnInit() {
     this.theme = this.config.get('theme');
     this.shuffle = this.config.getNumber('shuffle');
-
     this.padding = 2;
-
-    this.blocks = [...Array(15).keys(), null];
-
-    switch (this.config.getNumber('shuffle')) {
-      case Shuffle.Easy:
-        console.log('easy');
-        this.swap (14, 15);
-        break;
-      case Shuffle.Random:
-        this.shuffleArray(this.blocks);
-        console.log('random');
-        break;
-    }
-
+    this.game.shuffle(this.shuffle);
   }
 
   image(n: number): string {
-    if (this.blocks[n] !== null ) {
-      return this.imagesBaseDir + '/' + this.config.get('theme') + '/' + this.blocks[n] + '.jpg';
+    if (this.game.blocks[n] !== null ) {
+      return this.imagesBaseDir + '/' + this.config.get('theme') + '/' + this.game.blocks[n] + '.jpg';
     } else {
       return '';
     }
   }
 
   onclick(n: number) {
-    for (const i of this.neighbors((n))) {
-      if (this.blocks[i] == null) {
-        this.swap(n, i);
-        this.checkSolved();
-      }
+    this.game.move(n);
+    if (this.game.isSolved()) {
+      this.padding = 0;
     }
-  }
-
-  neighbors(n: number): number[] {
-    const result = [];
-    if (n % 4 !== 0) {
-      result.push(n - 1);
-    }
-    if (n % 4 !== 3) {
-      result.push(n + 1);
-    }
-    if (n > 3) {
-      result.push(n - 4);
-    }
-    if (n < 12) {
-      result.push(n + 4);
-    }
-    return result;
-  }
-
-  shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  }
-
-  private swap(n: number, i: number) {
-    const tmp = this.blocks[n];
-    this.blocks[n] = this.blocks[i];
-    this.blocks[i] = tmp;
-  }
-
-  private checkSolved() {
-    for (let i = 0; i < 15; i++) {
-      if (this.blocks[i] !== i) { return; }
-    }
-    this.blocks[15] = 15;
-    this.padding = 0;
   }
 
   reset() {
@@ -114,6 +59,5 @@ export class SlidingPuzzleComponent implements OnInit {
 
   onShuffleChange() {
     this.config.setNumber('shuffle', this.shuffle);
-    this.reset();
   }
 }
